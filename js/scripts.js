171783d2a794d7caaa489f1594eb6495830aebfb
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-03-19 20:31:33 -0300

    Se termino de hacer la animacion de las clases de aventurero y se agrego owl carousel tanto como jquery

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,13 @@ $(document).ready(function(){
     items:2,
     loop:true,
     margin:10,
+    mouseDrag:true,
+    touchDrag:true,
+    nav: true,
     responsive:{
+        320:{
+            items:1
+        },
         600:{
             items:4
         }
